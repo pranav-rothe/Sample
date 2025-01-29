cce4a10af6dc81922a047978385bee400662c9a5
pranav-rothe <pranavrothe96@gmail.com>
2025-01-29 18:28:40 +0530

    Update ClassOne.java
    new print statement added

--- src/test/java/demo/ClassOne.java
+++ src/test/java/demo/ClassOne.java
@@ -5,6 +5,8 @@ public class ClassOne {
 	public static void main(String[] args) {
 
 		System.out.println("Hello we are working on automation testing.");
+
+		System.out.println("Changes from gitHub remote");
 	}
 
 }
