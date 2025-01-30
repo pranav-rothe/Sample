67bc09de4bc3bf981a307e8d84d2817119668db7
pranav-rothe <pranavrothe96@gmail.com>
2025-01-30 11:28:03 +0530

    Thursday Commit

---
+++ src/test/java/demo/ClassFive.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class ClassFive {
+
+	public static void main(String[] args) {
+
+		System.out.println("new line added to remote git");
+	}
+
+}
--- src/test/java/demo/ClassFour.java
+++ src/test/java/demo/ClassFour.java
@@ -5,6 +5,8 @@ public class ClassFour {
 	public static void main(String[] args) {
 
 		System.out.println("New four class created");
+		
+		System.out.println("Hi GitHub");
 	}
 
 }
